b91662c5694d1b1864284c3e9606fdb35ca6de34
Fausto Roger <hixian@gmail.com>
2023-01-09 17:57:08 -0300

    add terraform code for section 05-02

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c2-variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "Region in which AWS resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_keypair" {
+  description = "AWS EC2 Keypair that need to be associated with EC2 instance"
+  type        = string
+  default     = "terraform-key"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_type_list" {
+  description = "EC2 Instance type"
+  type        = list(string)
+  default     = ["t3.micro", "t3.small"]
+}
+variable "instance_type_map" {
+  description = "EC2 Instance type"
+  type        = map(string)
+  default = {
+    "dev"  = "t3.micro"
+    "qa"   = "t3.small"
+    "prod" = "t3.large"
+  }
+}
